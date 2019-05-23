3be39a42cf1ade3c1bfe8e68c715786924b4f2e1
nee32 <740126220@qq.com>
2019-05-23 17:12:34 +0800

    fix 优化

--- QzoneLogin/Form1.Designer.cs
+++ QzoneLogin/Form1.Designer.cs
@@ -28,22 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.login = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(128, 22);
+            this.textBox1.Location = new System.Drawing.Point(30, 21);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(436, 21);
+            this.textBox1.Size = new System.Drawing.Size(751, 21);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(628, 22);
+            this.login.Location = new System.Drawing.Point(903, 21);
             this.login.Name = "login";
             this.login.Size = new System.Drawing.Size(75, 23);
             this.login.TabIndex = 1;
@@ -51,30 +53,29 @@
             this.login.UseVisualStyleBackColor = true;
             this.login.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(39, 30);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Name";
-            // 
             // webBrowser1
             // 
-            this.webBrowser1.Location = new System.Drawing.Point(128, 77);
+            this.webBrowser1.Location = new System.Drawing.Point(30, 58);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(436, 262);
+            this.webBrowser1.Size = new System.Drawing.Size(606, 598);
             this.webBrowser1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(660, 58);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(318, 598);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1011, 697);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.webBrowser1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.login);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
@@ -89,8 +90,8 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button login;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- QzoneLogin/Form1.cs
+++ QzoneLogin/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,10 +10,10 @@ namespace QzoneLogin
 {
     public partial class Form1 : Form
     {
-        private List<KVClass> dic = new List<KVClass>();
-
-        string url = "https://xui.ptlogin2.qq.com/cgi-bin/xlogin?proxy_url=https%3A//qzs.qq.com/qzone/v6/portal/proxy.html&daid=5&&hide_title_bar=1&low_login=0&qlogin_auto_login=1&no_verifyimg=1&link_target=blank&appid=549000912&style=22&target=self&s_url=https%3A%2F%2Fqzs.qzone.qq.com%2Fqzone%2Fv5%2Floginsucc.html%3Fpara%3Dizone&pt_qr_app=%E6%89%8B%E6%9C%BAQQ%E7%A9%BA%E9%97%B4&pt_qr_link=http%3A//z.qzone.com/download.html&self_regurl=https%3A//qzs.qq.com/qzone/v6/reg/index.html&pt_qr_help_link=http%3A//z.qzone.com/download.html&pt_no_auth=1";
+        private static List<QQLoginModel> data = new List<QQLoginModel>();
+        private static string url = "https://xui.ptlogin2.qq.com/cgi-bin/xlogin?proxy_url=https://qzs.qq.com/qzone/v6/portal/proxy.html&daid=5&&hide_title_bar=1&low_login=0&qlogin_auto_login=1&no_verifyimg=1&link_target=blank&appid=549000912&style=22&target=self&s_url=https:%2F%2Fqzs.qzone.qq.com%2Fqzone%2Fv5%2Floginsucc.html%3Fpara%3Dizone&pt_qr_app=%E6%89%8B%E6%9C%BAQQ%E7%A9%BA%E9%97%B4&pt_qr_link=http://z.qzone.com/download.html&self_regurl=https://qzs.qq.com/qzone/v6/reg/index.html&pt_qr_help_link=http://z.qzone.com/download.html&pt_no_auth=1";
         private static int index = 0;
+        private static List<string> successList = new List<string>();
 
         public Form1()
         {
@@ -21,80 +22,58 @@ namespace QzoneLogin
             foreach (var item in text)
             {
                 var array = item.Split(new string[] { "\t" }, StringSplitOptions.RemoveEmptyEntries);
-                dic.Add(new KVClass() {
-                     key= array[0],
-                     value= array[1]
-                });
+                data.Add(new QQLoginModel() { qq = array[0], pwd = array[1] });
             }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             webBrowser1.ScriptErrorsSuppressed = true;
             webBrowser1.Navigate(url);
-            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
         }
-
-        private void QQLogin(string QQ, string Pwd)
-        {
-            System.Security.Cryptography.MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
-            string QQPwd = Convert.ToBase64String(md5.ComputeHash(System.Text.Encoding.UTF8.GetBytes(Pwd)));
-            System.Diagnostics.Process pro = new System.Diagnostics.Process();
-            pro.StartInfo.FileName = @"D:\Program Files (x86)\Tencent\QQ\Bin\QQ.exe";
-            pro.StartInfo.Arguments = "/start QQUIN:" + QQ + " PWDHASH:" + QQPwd + " /stat:40"; pro.Start(); }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            Login(dic[index].key, dic[index].value);
-            index++;
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            Login();
         }
 
         private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
+            if (webBrowser1.ReadyState != WebBrowserReadyState.Complete) return;
+
+            //再次判断是否加载完成
             if (webBrowser1.ReadyState == WebBrowserReadyState.Complete && webBrowser1.IsBusy == false)
             {
+                //while (doc == null)  //网络操作总是伴随着一些不可预知的异常，所以在这以防万一对象为空,我们继续判断
+                //{
+                //    Application.DoEvents();//如果为空，就转交控制权
+                //}
 
-            }
-
-
-            //为了保险起见 我们在这再次判断是否加载完成
-            if (webBrowser1.ReadyState == WebBrowserReadyState.Complete && webBrowser1.IsBusy == false)
-            {
-                HtmlDocument doc = webBrowser1.Document; //抓取网页
-                while (doc == null)  //网络操作总是伴随着一些不可预知的异常，所以在这以防万一对象为空,我们继续判断
+                if (e.Url.AbsoluteUri == "https://user.qzone.qq.com/" + data[index].qq)
                 {
-                    Application.DoEvents();//如果为空，就转交控制权
+                    if (!successList.Contains(data[index].qq))
+                    {
+                        successList.Add(data[index].qq);
+                        richTextBox1.AppendText($"==={data[index].qq}登录完成===" + "\n");
+                        index++;
+                    }
+                    webBrowser1.Navigate(url);
                 }
 
-
-                for (int i = 0; i < dic.Count; i++)
+                else if (e.Url.AbsoluteUri == "https://qzs.qq.com/ac/qzfl/release/resource/html/storage_helper.html")
                 {
-                    
+                    Thread.Sleep(500);
+                    webBrowser1.Navigate(url);
+                    return;
                 }
             }
-            else
-            {
-
-            }
-
-            if (webBrowser1.ReadyState < WebBrowserReadyState.Complete) return;
-
-            if (e.Url.AbsoluteUri == "https://qzs.qq.com/ac/qzfl/release/resource/html/storage_helper.html")
-            {
-                Thread.Sleep(2000);
-                webBrowser1.Navigate(url);
-                return;
-            }
-            else
-            {
-
-            }
+            
         }
 
-
-        private void Login(string QQ, string Pwd)
+        private void Login()
         {
-            HtmlDocument doc = this.webBrowser1.Document;
+            HtmlDocument doc = webBrowser1.Document; //抓取网页
             doc.GetElementById("login_button")?.InvokeMember("click");
             doc.GetElementById("switcher_plogin")?.InvokeMember("click");//切换成账号密码登录
             Thread.Sleep(500);
@@ -102,19 +81,19 @@ namespace QzoneLogin
             var pwd = doc.GetElementById("p");
 
             ////设置元素value属性值 (用户名 密码值)
-            user.SetAttribute("value", QQ);
-            pwd.SetAttribute("value", Pwd);
+            user.SetAttribute("value", data[index].qq);
+            pwd.SetAttribute("value", data[index].pwd);
             //Thread.Sleep(1000);
             ////执行元素的方法：如click submit
             doc.GetElementById("login_button")?.InvokeMember("click");
         }
-
     }
 
-    public class KVClass
-    {
-        public string key { get; set; }
 
-        public string value { get; set; }
+    public class QQLoginModel
+    {
+        public string qq { get; set; }
+
+        public string pwd { get; set; }
     }
 }
